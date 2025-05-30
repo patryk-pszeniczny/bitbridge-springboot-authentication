5cc0c1e8c6b401ff01367025faf7294c7a1df610
Patryk Pszeniczny <vcode1@onet.pl>
2025-05-30 23:19:22 +0200

    commit: rebranch

--- src/main/java/bitbridge/authentication/service/UserDetailsImpl.java
+++ src/main/java/bitbridge/authentication/service/UserDetailsImpl.java
@@ -1,38 +1,19 @@
 package bitbridge.authentication.service;
 
-import bitbridge.authentication.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
-
     private UUID id;
     private String username;
     private String email;
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
-
-    public static UserDetailsImpl build(User user) {
-        List<GrantedAuthority> authorities = user.getRoles().stream()
-                .map(role -> new SimpleGrantedAuthority(role))
-                .collect(Collectors.toList());
-
-        return new UserDetailsImpl(
-                user.getId(),
-                user.getUsername(),
-                user.getEmail(),
-                null, // password not needed for OAuth2
-                authorities);
-    }
-
 }
