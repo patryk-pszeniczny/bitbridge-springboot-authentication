acf11012c98f1fa07aedda69844c2f7f69871831
Patryk Pszeniczny <vcode1@onet.pl>
2025-06-15 20:37:22 +0200

    commit: Prevent relational issues in UserModel by restricting equals/hashCode reason: Use onlyExplicitlyIncluded = true to avoid problems with Set<String> roles

--- src/main/java/bitbridge/authentication/domain/model/User.java
+++ src/main/java/bitbridge/authentication/domain/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Setter
 @Entity
 @Table(name = "users")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
     @Id
