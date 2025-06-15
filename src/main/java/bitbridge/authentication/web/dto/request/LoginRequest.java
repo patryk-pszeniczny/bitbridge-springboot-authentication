aff4722e43dae875c056df26cbe87be9382f5e75
Patryk Pszeniczny <vcode1@onet.pl>
2025-06-15 23:01:10 +0200

    Refactor LoginRequest and ProviderRequest to include no-args and all-args constructors

--- src/main/java/bitbridge/authentication/web/dto/request/LoginRequest.java
+++ src/main/java/bitbridge/authentication/web/dto/request/LoginRequest.java
@@ -1,9 +1,13 @@
 package bitbridge.authentication.web.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank
     private String email;
--- src/main/java/bitbridge/authentication/web/dto/request/ProviderRequest.java
+++ src/main/java/bitbridge/authentication/web/dto/request/ProviderRequest.java
@@ -1,9 +1,13 @@
 package bitbridge.authentication.web.dto.request;
 
 import bitbridge.authentication.domain.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProviderRequest {
     private String providerId;
     private User user;
