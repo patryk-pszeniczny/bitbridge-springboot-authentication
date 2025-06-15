7275bdb8ceb69daa23a67864c2bd4e5f91ec37a7
Patryk Pszeniczny <vcode1@onet.pl>
2025-06-15 22:14:08 +0200

    Disable security filters in AuthControllerTest for isolated testing

--- src/test/java/bitbridge/authentication/web/controller/AuthControllerTest.java
+++ src/test/java/bitbridge/authentication/web/controller/AuthControllerTest.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
 
     @Autowired
