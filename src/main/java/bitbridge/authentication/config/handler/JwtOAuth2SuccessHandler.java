8c08f3d40f98cdf504b56d5fe09ed8829bdb29f7
Patryk Pszeniczny <vcode1@onet.pl>
2025-06-15 20:32:01 +0200

    commit: JwtO2 Handlers

---
+++ src/main/java/bitbridge/authentication/config/handler/JwtOAuth2SuccessHandler.java
@@ -0,0 +1,27 @@
+package bitbridge.authentication.config.handler;
+
+import bitbridge.authentication.infrastructure.security.JwtService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtOAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        String token = jwtService.generateJwtToken(authentication);
+        response.sendRedirect("http://localhost:5173/?token=" + token);
+    }
+}
